75a57aeaac33dff0255598e8fd5fb61b004954fa
vktpcmmg <vk340387@gmail.com>
2025-07-24 20:33:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,80 +1,49 @@
 import streamlit as st
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+from oauth2client.service_account import ServiceAccountCredentials
 import os
-from datetime import datetime
+import datetime
 
-# Google Drive Authentication
+# Google Drive setup
+FOLDER_ID = "13hG9ayDAHfK9MdVXUQwAYyZlUqWMKKYK"  # your shared Google Drive folder ID
+CREDS_FILE = "credentials.json"
+
+scope = ['https://www.googleapis.com/auth/drive']
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
+gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDS_FILE, scope)
 drive = GoogleDrive(gauth)
 
-# Replace with your main folder ID in Google Drive
-PARENT_FOLDER_ID = '13hG9ayDAHfK9MdVXUQwAYyZlUqWMKKYK'
-
-# Create folder inside Google Drive
-def create_drive_folder(folder_name, parent_id):
-    folder_metadata = {
-        'title': folder_name,
-        'parents': [{'id': parent_id}],
-        'mimeType': 'application/vnd.google-apps.folder'
-    }
-    folder = drive.CreateFile(folder_metadata)
-    folder.Upload()
-    return folder['id']
-
-# Upload file to Google Drive
-def upload_file_to_drive(file, folder_id):
-    filename = file.name
-    filepath = os.path.join("/tmp", filename)
-    with open(filepath, "wb") as f:
-        f.write(file.getvalue())
-
-    drive_file = drive.CreateFile({'title': filename, 'parents': [{'id': folder_id}]})
-    drive_file.SetContentFile(filepath)
-    drive_file.Upload()
-    os.remove(filepath)
-
-# Create metadata text file and upload it
-def upload_metadata(folder_id, technician, zone, locality, site_name, remark):
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    content = f"""Technician: {technician}
-Zone: {zone}
-Locality: {locality}
-Site Name: {site_name}
-Remark: {remark}
-Date: {now}
-"""
-    filepath = "/tmp/info.txt"
-    with open(filepath, "w") as f:
-        f.write(content)
-
-    file_drive = drive.CreateFile({'title': "info.txt", 'parents': [{'id': folder_id}]})
-    file_drive.SetContentFile(filepath)
-    file_drive.Upload()
-    os.remove(filepath)
-
 # Streamlit UI
-st.title("Old Meter Photo Upload")
+st.title("Meter Replacement Photo Upload")
 
-with st.form("upload_form", clear_on_submit=True):
-    technician = st.text_input("Technician Name")
-    zone = st.selectbox("Zone", ["NZ", "CZ", "SZ", "MCZ", "SCZ", "EZ"])
-    locality = st.text_input("Locality")
-    site_name = st.text_input("Site Name (Building)")
-    remark = st.text_area("Remark (if any)")
+technician = st.text_input("Technician Name")
+zone = st.selectbox("Zone", ["NZ", "CZ", "SZ", "MCZ", "SCZ", "EZ"])
+locality = st.text_input("Locality")
+building = st.text_input("Building Name")
+remark = st.text_area("Remarks")
+uploaded_files = st.file_uploader("Upload Old Meter Photos", accept_multiple_files=True, type=["jpg", "jpeg", "png"])
 
-    photo_files = st.file_uploader("Upload Old Meter Photos", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
+if st.button("Upload Photos"):
+    if not all([technician, zone, locality, building, uploaded_files]):
+        st.warning("Please fill all required fields and upload at least one photo.")
+    else:
+        # Check or create subfolder for building
+        query = f"'{FOLDER_ID}' in parents and title = '{building}' and mimeType = 'application/vnd.google-apps.folder' and trashed=false"
+        folder_list = drive.ListFile({'q': query}).GetList()
 
-    submitted = st.form_submit_button("Upload")
-
-    if submitted:
-        if technician and zone and locality and site_name and photo_files:
-            building_folder_id = create_drive_folder(site_name, PARENT_FOLDER_ID)
-            upload_metadata(building_folder_id, technician, zone, locality, site_name, remark)
-            for photo in photo_files:
-                upload_file_to_drive(photo, building_folder_id)
-
-            st.success("All files and details uploaded successfully.")
+        if folder_list:
+            subfolder = folder_list[0]
         else:
-            st.error("Please fill all fields and upload at least one photo.")
+            subfolder_metadata = {'title': building, 'parents': [{'id': FOLDER_ID}], 'mimeType': 'application/vnd.google-apps.folder'}
+            subfolder = drive.CreateFile(subfolder_metadata)
+            subfolder.Upload()
+
+        for file in uploaded_files:
+            now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            filename = f"{technician}_{zone}_{locality}_{now}_{file.name}"
+            gfile = drive.CreateFile({'title': filename, 'parents': [{'id': subfolder['id']}]})
+            gfile.SetContentString(file.getvalue().decode("ISO-8859-1"))
+            gfile.Upload()
+
+        st.success("All photos uploaded successfully!")
